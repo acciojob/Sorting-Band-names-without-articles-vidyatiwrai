76da5cc2dacb5a5fde82b3b4228e952227d56019
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-13 17:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 //your code here
+const bands = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'Aerosmith'];
 
-  touristSpots .map(item => `<li>${item}</li>`).join('')
+// Define the strip function to remove common words from band names
+function strip(word) {
+const regex = /^(a |the |an )/i;
+return word.replace(regex, '').trim();
+}
+
+// Sort the bands array using the strip function to ignore common words
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1);
+
+// Update the DOM with the sorted band names
+const bandList = document.getElementById('bands');
+sortedBands.forEach(band => {
+const listItem = document.createElement('li');
+listItem.textContent = band;
+bandList.appendChild(listItem);
+});
